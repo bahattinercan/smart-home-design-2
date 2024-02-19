11276d279c62cda7564d94e192c1719e2c0824aa
Bahattin Ercan <92323340+bahattinercan@users.noreply.github.com>
2024-02-19 17:12:32 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: false,
         brightness: Brightness.dark,
         primarySwatch: MainColors.greenColor,
         tabBarTheme: TabBarTheme(
